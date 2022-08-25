e8daceefe1fe434cfd11f3ae48e76e2cd9ae94c2
Bharath <91299756+BharathKatabathuni@users.noreply.github.com>
2022-08-25 01:56:13 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from setuptools import setup
+
+setup(
+    name='se-hw1',
+    version='1.0',
+    packages=['code', 'test'],
+    url='https://github.com/hacker95-bot/SE-HW1',
+    license='MIT License',
+    author='Vinay Kumar Reddy Perolla',
+    author_email='reddyvinay1995@gmail.com',
+    description='Github repo for HW1 for SE course'
+)
