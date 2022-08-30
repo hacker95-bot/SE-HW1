7c85cc6e5562dc501916d004b8194083377cf895
vamsitadikonda <vamsitadikonda99@gmail.com>
2022-08-30 18:43:09 -0400

    Adding automatic versions in setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+import versioneer
+setup(
+    name='se-hw1',
+    version=versioneer.get_version(),
+    packages=['code', 'test'],
+    url='https://github.com/hacker95-bot/SE-HW1',
+    license='MIT License',
+    author='Vinay Kumar Reddy Perolla',
+    author_email='reddyvinay1995@gmail.com',
+    description='Github repo for HW1 for SE course'
+)
